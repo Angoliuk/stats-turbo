7d4814ad974bd4c9f09455145656df35b3ffa23e
Angoliuk <gamongara@gmail.com>
2023-09-13 14:32:04 +0300

    removed comments from api

--- apps/stats-api/src/index.ts
+++ apps/stats-api/src/index.ts
@@ -4,11 +4,6 @@ dotenv.config();
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { appRouter } from "./root";
 import { createTRPCContext } from "./trpc";
-const { expressHandler } = require("trpc-playground/handlers/express");
-// import { expressHandler } from "trpc-playground/handlers/express";
-// V rot about
-// import type {} from "@stats/shared-api";
-// import type {} from "@stats/prisma";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,26 +12,7 @@ app.use(express.json());
 // do i need that?
 // app.use(morgan("dev")); // for pretty logging
 
-// app.get("/", (req, res) => {
-//   res.send("hello, world!");
-// });
-
-// Do i need that?
-
 const TRPC_ENDPOINT = "/trpc";
-
-const TRPC_PLAYGROUND_ENDPOINT = "/trpc-playground";
-expressHandler({
-  trpcApiEndpoint: TRPC_ENDPOINT,
-  playgroundEndpoint: TRPC_PLAYGROUND_ENDPOINT,
-  router: appRouter,
-  // uncomment this if you're using superjson
-  request: {
-    superjson: true,
-  },
-}).then((handeler: any) => {
-  app.use(handeler);
-});
 
 app.use(
   TRPC_ENDPOINT,
--- apps/stats-api/src/root.ts
+++ apps/stats-api/src/root.ts
@@ -1,10 +1,6 @@
 import { statsRouter } from "./modules/stats";
 import { createRouter } from "./trpc";
 
-// // V rot about
-// import type {} from "@stats/shared-api";
-// import type {} from "@stats/prisma";
-
 export const appRouter = createRouter({
   stats: statsRouter,
 });
