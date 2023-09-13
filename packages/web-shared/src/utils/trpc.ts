0dcb376d4b6503cb9537471881d2dd39d1e14004
Angoliuk <gamongara@gmail.com>
2023-09-13 14:33:00 +0300

    added shared utils

---
+++ packages/web-shared/src/utils/trpc.ts
@@ -0,0 +1,8 @@
+import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
+import type { AppRouter } from "@stats/api/root";
+import { createTRPCReact } from "@trpc/react-query";
+
+export const trpc = createTRPCReact<AppRouter>();
+
+export type RouterInputs = inferRouterInputs<AppRouter>;
+export type RouterOutputs = inferRouterOutputs<AppRouter>;
