c5719015bfe4c275fe4cb8456c8cd2f12d4a7124
Angoliuk <gamongara@gmail.com>
2023-09-13 14:33:08 +0300

    added shared svgs

---
+++ packages/web-shared/src/svg/index.ts
@@ -0,0 +1,4 @@
+export { ReactComponent as CupIcon } from "../../assets/svg/cup.svg";
+export { ReactComponent as MedalIcon } from "../../assets/svg/medal.svg";
+export { ReactComponent as UserIcon } from "../../assets/svg/user.svg";
+export { ReactComponent as SwordsIcon } from "../../assets/svg/swords.svg";
