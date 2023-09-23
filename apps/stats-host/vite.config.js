1ae1e4923ce02a23b573028a64178d6564aa6fd1
StanislavAnholiuk <stanislav.anholiuk@devit.group>
2023-09-23 21:14:36 +0300

    vite configs update

--- apps/stats-host/vite.config.js
+++ apps/stats-host/vite.config.js
@@ -1,19 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 import federation from "@originjs/vite-plugin-federation";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    svgr(),
     federation({
       name: "app",
       remotes: {
-        remoteApp: "http://localhost:5522/assets/remoteEntry.js",
+        "@stats/remote": "http://localhost:5522/assets/remoteEntry.js",
       },
       shared: ["react", "react-dom"],
     }),
   ],
+  resolve: {
+    alias: {
+      "@stats/host": path.resolve(__dirname, "./src/"),
+      "@stats/shared-web": path.resolve(__dirname, "../../packages/web-shared/dist/"),
+    },
+  },
   build: {
     modulePreload: false,
     target: "esnext",
--- apps/stats-remote/vite.config.ts
+++ apps/stats-remote/vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 export default defineConfig({
   plugins: [
     react(),
+    svgr(),
     federation({
       name: "remote_app",
       filename: "remoteEntry.js",
@@ -14,6 +17,12 @@ export default defineConfig({
       shared: ["react", "react-dom"],
     }),
   ],
+  resolve: {
+    alias: {
+      "@stats/remote": path.resolve(__dirname, "./src/"),
+      "@stats/shared-web": path.resolve(__dirname, "../../packages/web-shared/dist/"),
+    },
+  },
   build: {
     modulePreload: false,
     target: "esnext",
